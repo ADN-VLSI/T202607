2a05e5cf36ca54ad8319e730967889adafd5472f
ahzuch <chaudhuryuzair@gmail.com>
2026-08-19 10:58:12 +0600

    completed fifo.sv

--- source/fifo.sv
+++ source/fifo.sv
@@ -21,11 +21,28 @@ module fifo #(
     input  logic [DATA_WIDTH-1:0] data_in_i,
     output logic                  data_in_ready_o, data_out_valid_o,
     output logic [DATA_WIDTH-1:0] data_out_o,
-    output logic [ADDR_WIDTH:0]   count_o
+    output logic [ADDR_WIDTH:0]   count_o 
 );
 
     logic [DATA_WIDTH-1:0] mem [2**ADDR_WIDTH];
-    logic [ADDR_WIDTH:0] wp, rp, count;
+    logic [ADDR_WIDTH:0] wp, rp, count; // count shows the number of slots that are full
+
+    assign count_o = count; 
+
+    always_comb begin 
+        if (count != 2**ADDR_WIDTH) begin
+            data_in_ready_o = 1'b1;
+        end else begin
+            data_in_ready_o = 1'b0;
+        end
+
+        if (count != 0) begin
+            data_out_valid_o = 1'b1;
+        end else begin
+            data_out_valid_o = 1'b0;
+        end 
+    end
+
 
     always_ff @(posedge clk_i or negedge arst_ni) begin
         if (!arst_ni) begin
@@ -46,7 +63,7 @@ module fifo #(
         end else if (data_in_valid_i && data_in_ready_o) begin
             count <= count + 1;
         end else if (data_out_valid_o && data_out_ready_i) begin
-            count <= count + 1;        
+            count <= count - 1;        
         end
     end
 
