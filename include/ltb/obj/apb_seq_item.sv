0b9f7ea62c563d108157dde04a098fa4cf078e84
shantanupaulee-png <shantanu.paul.ee@gmail.com>
2026-09-15 16:15:41 +0600

    constraints

--- include/ltb/obj/apb_seq_item.sv
+++ include/ltb/obj/apb_seq_item.sv
@@ -1,19 +1,48 @@
-
 `include "uart_regif_pkg.sv"
 
 class apb_seq_item;
 
-  rand logic        we;
-  rand logic [ 4:0] addr;
-  rand logic [31:0] data;
+    rand logic        we;
+    rand logic [4:0]  addr;
+    rand logic [31:0] data;
 
-  bit               allow_random_addr = 0;
+    bit allow_random_addr = 0;
 
-  constraint addr_c {
-    if (!allow_random_addr) {
-      addr inside {uart_regif_pkg::ADDR_CTRL, uart_regif_pkg::ADDR_CFG, uart_regif_pkg::ADDR_STATUS,
-                   uart_regif_pkg::ADDR_TXD, uart_regif_pkg::ADDR_RXD, uart_regif_pkg::ADDR_INTR};
+    constraint addr_c {
+        if (!allow_random_addr) {
+            addr inside {
+                uart_regif_pkg::ADDR_CTRL,
+                uart_regif_pkg::ADDR_CFG,
+                uart_regif_pkg::ADDR_STATUS,
+                uart_regif_pkg::ADDR_TXD,
+                uart_regif_pkg::ADDR_RXD,
+                uart_regif_pkg::ADDR_INTR
+            };
+        }
     }
-  }
+
+    constraint addr_access_c {
+
+        // Write transaction
+        if (we) {
+            addr inside {
+                uart_regif_pkg::ADDR_CTRL,
+                uart_regif_pkg::ADDR_CFG,
+                uart_regif_pkg::ADDR_TXD,
+                uart_regif_pkg::ADDR_INTR
+            };
+        }
+
+        // Read transaction
+        else {
+            addr inside {
+                uart_regif_pkg::ADDR_CTRL,
+                uart_regif_pkg::ADDR_CFG,
+                uart_regif_pkg::ADDR_STATUS,
+                uart_regif_pkg::ADDR_RXD,
+                uart_regif_pkg::ADDR_INTR
+            };
+        }
+    }
 
 endclass
